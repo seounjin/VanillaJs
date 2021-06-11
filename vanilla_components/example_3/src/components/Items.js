1996f5a779ecc38192afce5257c3b4e5a439d296
seounjin <invanda7@gmail.com>
2021-06-11 18:49:17 +0900

    example_3(component)

--- vanilla_components/example_3/src/components/Items.js
+++ vanilla_components/example_3/src/components/Items.js
@@ -1,42 +1,83 @@
 import Component from "../core/Component.js";
 
-// case 2
-// 상위컴포넌트에 바인딩
+
+
+// case 3
+
 export default class Items extends Component {
-    setup () {
-      this.$state = { items: ['item1', 'item2'] };
-    }
-    template () {
-      const { items } = this.$state;
-      return `
-        <ul>
-          ${items.map((item, index) => 
-              `<li>
-                  ${item}
-                  <button class="deleteBtn" data-index=${index}>삭제</button>
-              </li>`).join('')}
-        </ul>
-        <button class="addBtn">추가</button>
-      `
-    }
-  
-    setEvent () {
-        // 모든 이벤트를 this.$target에 등록하여 사용하면 된다.
-        this.$target.addEventListener('click', ({ target }) => {
-          const items = [ ...this.$state.items ];
-    
-          if (target.classList.contains('addBtn')) {
-            this.setState({ items: [ ...items, `item${items.length + 1}` ] });
-          }
-    
-          if (target.classList.contains('deleteBtn')) {
-            items.splice(target.dataset.index, 1);
-            this.setState({ items });
-          }
-    
-        });
-      }
+
+  template() {
+    const { filteredItems } = this.$props;
+    console.log("filteredItems",filteredItems)
+    return `
+      <ul>
+        ${filteredItems.map(({contents, active, seq}) => `
+          <li data-seq="${seq}">
+            ${contents}
+            <button class="toggleBtn" style="color: ${active ? '#09F' : '#F09'}">
+              ${active ? '활성' : '비활성'}
+            </button>
+            <button class="deleteBtn">삭제</button>
+          </li>
+        `).join('')}
+      </ul>
+    `
+  }
+
+  setEvent() {
+    const { deleteItem, toggleItem } = this.$props;
+
+    this.addEvent('click', '.deleteBtn', ({target}) => {
+      deleteItem(Number(target.closest('[data-seq]').dataset.seq));
+    });
+
+    this.addEvent('click', '.toggleBtn', ({target}) => {
+      toggleItem(Number(target.closest('[data-seq]').dataset.seq));
+    });
+
+  }
+
 }
+
+
+
+// // case 2
+// // 상위컴포넌트에 바인딩
+// export default class Items extends Component {
+//     setup () {
+//       this.$state = { items: ['item1', 'item2'] };
+//     }
+//     template () {
+//       const { items } = this.$state;
+//       return `
+//         <ul>
+//           ${items.map((item, index) => 
+//               `<li>
+//                   ${item}
+//                   <button class="deleteBtn" data-index=${index}>삭제</button>
+//               </li>`).join('')}
+//         </ul>
+//         <button class="addBtn">추가</button>
+//       `
+//     }
+  
+//     setEvent () {
+//         // 모든 이벤트를 this.$target에 등록하여 사용하면 된다.
+//         this.$target.addEventListener('click', ({ target }) => {
+//           const items = [ ...this.$state.items ];
+    
+//           if (target.classList.contains('addBtn')) {
+//             this.setState({ items: [ ...items, `item${items.length + 1}` ] });
+//           }
+    
+//           if (target.classList.contains('deleteBtn')) {
+//             items.splice(target.dataset.index, 1);
+//             this.setState({ items });
+//           }
+    
+//         });
+//       }
+// }
 
 
 // case 1
--- vanilla_components/example_3/src/core/Component.js
+++ vanilla_components/example_3/src/core/Component.js
@@ -1,20 +1,45 @@
 export default class Component {
 
-    constructor ($target) {
+    constructor ($target, $props) {
       this.$target = $target;
+      this.$props = $props; // case 3
+
       this.setup();
       this.render();
       this.setEvent(); // case 2  한번만 실행
     }
+
     setup () {};
+
+    mounted () {}; // case 3
+
     template () { return ''; }
+
     render () {
       this.$target.innerHTML = this.template();
     //   this.setEvent(); // case 1 랜더링 될때마다 실행
+      this.mounted(); // render 후에 mounted가 실행 된다.
     }
+
     setEvent () {}
+    
     setState (newState) {
       this.$state = { ...this.$state, ...newState };
       this.render();
     }
+
+    addEvent (eventType, selector, callback) { //case3
+
+      const children = [ ...this.$target.querySelectorAll(selector) ]; 
+
+      const isTarget = (target) => children.includes(target)
+                                   || target.closest(selector);
+
+      this.$target.addEventListener(eventType, event => {
+        if (!isTarget(event.target)) return false;
+        callback(event);
+      })
+    }
+  
+  
 }
