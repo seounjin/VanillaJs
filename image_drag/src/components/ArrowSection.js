27efc7d872b799a64a8f7891d5d298b395f434b3
seounjin <invanda7@gmail.com>
2021-06-26 12:41:14 +0900

    arrow container update...

--- image_drag/src/components/ArrowSection.js
+++ image_drag/src/components/ArrowSection.js
@@ -20,16 +20,25 @@ export default class ArrowSection extends View{
         
         // clientWidth 실제 컨네이너 크기
         if (classListContainer.clientWidth < (liList.length * 260 + Number(dataPosition))) {
+            
             // 오른쪽으로 이동
             dataPosition = Number(dataPosition) - 260;
             
+            //실제 보여지는 classListContainer 크기보다 카드의 개수가 작을경우 오른쪽 arrow 버튼 비활성화
             if (classListContainer.clientWidth > (liList.length * 260 + Number(dataPosition))) {
                 slideNext.style.color = '#cfd8dc';
                 slideNext.classList.remove('slide-next-hover');
+            } else {
+                //오른쪽 arrow버튼 활성화
+                slideNext.style.color = '#2f3059';
+                slideNext.classList.add('slide-next-hover');
+
+                //왼쪽 arrow버튼 활성화
+                slidePrev.style.color = '#2f3059';
+                slidePrev.classList.add('slide-prev-hover');
+
             }
-            //오른쪽 arrow버튼 활성화
-            slideNext.style.color = '#2f3059';
-            slideNext.classList.add('slide-next-hover');
+            
 
         } 
 
@@ -40,30 +49,36 @@ export default class ArrowSection extends View{
 
     }));
 
-
+        // 왼쪽 arrow 버튼
         const slidePrev = this.element.querySelector('.slide-prev');
         
         slidePrev.addEventListener('click', ((event) => {
         
-        // ul 태그
+            // ul 태그
             const classListContainer = event.target.parentElement.parentElement.nextElementSibling;
             const classList = classListContainer.childNodes[0];
             let dataPosition = classList.getAttribute('data-position');
             
-            
+            // 왼쪽에 안보이는 카드가 존재할 경우 dataPosition은 0보다 작으므로 왼쪽으로 이동 가능
             if (Number(dataPosition) < 0) {
                 dataPosition = Number(dataPosition) + 260;
 
                 slidePrev.style.color = '#2f3059';
                 slidePrev.classList.add('slide-prev-hover');
                 
+                //오른쪽 arrow버튼 활성화
+                slideNext.style.color = '#2f3059';
+                slideNext.classList.add('slide-next-hover');
+                
+                // dataPosition 0이면 시작점이므로 왼쪽에 카드가 없음
                 if (Number(dataPosition) === 0) {
                     slidePrev.style.color = '#cfd8dc';
                     slidePrev.classList.remove('slide-pre-hover');
                     
                 }
+            } 
             
-            }
+            
         
         classListContainer.style.transition = 'transform 1s';
         classListContainer.style.transform = 'translateX(' + String(dataPosition) + 'px)';
