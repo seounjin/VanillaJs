2f197e72a219078478065331d62896ce37f95efd
seounjin <invanda7@gmail.com>
2021-07-03 21:58:06 +0900

    data add

---
+++ image_collection/src/model/dummyData.js
@@ -0,0 +1,24 @@
+
+
+export const data = [
+    
+    {
+        "id":"1",
+        "name":"아이린",
+        "type":"DIRECTORY",
+        "filePath": './img/Irene.jpg',
+        
+    },
+
+    {
+        "id":"2",
+        "name":"아이린 베걍",
+        "type":"DIRECTORY",
+        "filePath": './img/Irene.jpg',
+        "parent": {
+            "id": "1"
+        }
+    },
+
+
+]
